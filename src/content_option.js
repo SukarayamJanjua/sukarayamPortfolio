8c28be639021a1d3f7d4f7841f59f14c34e7a534
Sukarayam Janjua <94704551+SukarayamJanjua@users.noreply.github.com>
2023-03-03 16:40:50 +0530

    My portfolio website

---
+++ src/content_option.js
@@ -0,0 +1,177 @@
+const logotext = "SUKARAYAM";
+const meta = {
+    title: "Sukarayam",
+    description: "I’m Sukarayam Janjua gamer _ Full stack devloper,currently studing at NITJ, India",
+};
+
+const introdata = {
+    title: "I’m Sukarayam Janjua",
+    animated: {
+        first: "I love coding",
+        second: "I develop websites",
+        third: "I build games",
+        fourth: "I love Open-source",
+    },
+    description: "Currently pursuing btech at NIT Jalandhar,India. I'll buy you a coffee if you will debug my code",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+    // your_img_url: "https://pbs.twimg.com/media/Fp2EXU4aQAAroAt?format=jpg&name=900x900",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "Currently 20 , I'm a student at NITJ, India. A passionate Web developer, Game developer, 3d modeller, C++ coder, Video editor.Always open to learn something new and help grow the community.Video games are my love and coding is my passion.Apart from these my hobbies includes playing Instrumental Music, parkour, Novels, football "
+};
+const worktimeline = [{
+        jobtitle: "Designer of week",
+        where: "YAdfi",
+        date: "2020",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "Jamalya",
+        date: "2019",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "ALquds",
+        date: "2019",
+    },
+];
+
+const skills = [{
+        name: "C++",
+        value: 85,
+    },
+    {
+        name: "Game-dev",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 70,
+    },
+    {
+        name: "React",
+        value: 80,
+    },
+    {
+        name: "3d Modelling",
+        value: 70,
+    },
+    {
+        name: "Open source",
+        value: 60,
+    }
+];
+
+const services = [{
+        title: "Websites",
+        description: "Full stack website development using major tech stack React, tailwindcss, redux, firebase",
+    },
+    {
+        title: "Games",
+        description: "Building and exporting games made in Unity and C#",
+    },
+    {
+        title: "Frontent Design",
+        description: "Expertise in frontent desgin may be through html, css, Js or may it be React or wordpress or Next.js",
+    },
+    {
+        title: "Video Editor",
+        description: "Editing smooth and professional looking videos using software Adobe premiere pro",
+    },
+];
+
+const dataportfolio = [{
+        // img: "https://brand.linkedin.com/etc.clientlibs/settings/wcm/designs/gandalf/clientlibs/resources/images/og-social-share-image.jpg",
+        img: "https://clubrunner.blob.core.windows.net/00000050169/Images/LinkedInSquareLogo.jpg.png",
+        desctiption: "LinkedIn Clone made in React.js, CSS, Firebase, Redux ",
+        link: "https://github.com/SukarayamJanjua/LinkedIn-Clone",
+    },
+    {
+        img: "https://img.iproperty.com.my/angel/750x1000-fit/wp-content/uploads/sites/2/2020/05/119619406_s.jpg",
+        desctiption: "A plant delivery app that uses google maps api to find nearest nursery",
+        link: "https://github.com/SukarayamJanjua/ThePlantFactory",
+    },
+   
+    {
+        img: "https://timesofindia.indiatimes.com/thumb/msid-80131937,width-1200,height-900,resizemode-4/.jpg",
+        desctiption: "A hackathon project providing affordable education to all",
+        link: "https://github.com/SukarayamJanjua/Hacknights",
+    },
+    {
+        img: "https://sharpcoderblog.com/public/images/018/0182a08aa94ee0f2b8472411dacc50ef.jpg",
+        desctiption: "Endless runner game made in Unity and C#",
+        link: "https://github.com/SukarayamJanjua/EndlessRunnerGame",
+    },
+    {
+        img: "https://mir-s3-cdn-cf.behance.net/projects/404/126bfb101811857.Y3JvcCwxMDA3LDc4OCwxOTcsMA.png",
+        desctiption: "Still Life 3d scene made with Blender",
+        link: "https://github.com/SukarayamJanjua/3dproject_StillLife",
+    },
+    {
+        img: "https://img.freepik.com/premium-vector/malware-ransomware-virus_34089-140.jpg",
+        desctiption: "A Ransomeware virus made in python",
+        link: "https://github.com/SukarayamJanjua/Ransomware",
+    },
+    {
+        img: "https://media.licdn.com/dms/image/C4D22AQHq4V9H_mBIkw/feedshare-shrink_800/0/1676749782961?e=1680739200&v=beta&t=yQJS9AtC_-sTQ4bLJFJrFT_biL2xHgFe-DPPBt66XLQ",
+        desctiption: "Successful completion of hactoberfest 2022",
+        link: "https://github.com/SukarayamJanjua",
+    },
+    // {
+    //     img: "https://picsum.photos/400/300/?grayscale",
+    //     desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/?grayscale",
+    //     desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/550/?grayscale",
+    //     desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/?grayscale",
+    //     desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/700/?grayscale",
+    //     desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: " agam.sukarayam@gmail.com",
+    YOUR_FONE: "(+91) 9478760755",
+    description: "Feel free to get in touch with me anytime via any of the given contact points.You can follow me on Github, LinkedIn, Twitter, instagram as well to stay connected",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/SukarayamJanjua",
+    facebook: "https://www.instagram.com/agam._216/",
+    linkedin: "https://www.linkedin.com/in/sukarayam-janjua/",
+    twitter: "https://twitter.com/sukarayam",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
